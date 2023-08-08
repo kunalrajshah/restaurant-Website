f24cb0fe2f584058d55ddb3232abf52973b71cd8
kunal raj shah <rajkunal7279@gmail.com>
2023-08-09 00:10:03 +0530

    adding context Api for updating badge number just add not function

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import Icon from '../Cart/cartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../Store/Cart-Context';
 
 const HeaderCartButton = (props) => { 
+  const CartCtx=useContext(CartContext);
+  const numberOfCartItems=CartCtx.items.reduce((curNumber,item)=>{
+    return curNumber + item.amount;
+  },0)
   return (
     <button className={classes.button} onClick={props.onclk} >
       <span className={classes.icon}><Icon /></span>
       <span>Add to Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
