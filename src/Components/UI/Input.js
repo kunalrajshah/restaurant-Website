19753f27b278a6c1e43b0232db1c74a2268b9579
kunal raj shah <rajkunal7279@gmail.com>
2023-08-08 21:59:22 +0530

    Adding a cart without any Function

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Classes from './Input.module.css';
+import React from "react";
+import Classes from "./Input.module.css";
 
 const Input = (props) => {
   return (
     <div className={Classes.input}>
-   <label>{props.label}</label>
-   {/* in below sntax {...props.input} it take all the value of input here */}
-   <input {...props.input}/>  
-   </div>
+      <label>{props.label}</label>
+      {/* in below sntax {...props.input} it take all the value of input here */}
+      <input {...props.input} />
+    </div>
   );
-}
+};
 
 export default Input;
---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import ReactDom from "react-dom";
+import Classes from "./Modal.module.css";
+
+const Modal = (props) => {
+  // For react POrtal
+  const BackDrop = () => (
+     <div className={Classes.backdrop}></div>
+  );
+
+  const ModalOverlay = (props) => (
+    <div className={Classes.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+
+
+  return (
+    <Fragment>
+      {ReactDom.createPortal(<BackDrop />, document.getElementById('overlay'))}
+      {ReactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById('overlay')
+      )}
+      {/* <BackDrop />
+      <ModalOverlay>{props.children}</ModalOverlay> */}
+    </Fragment>
+  );
+};
+
+export default Modal;
